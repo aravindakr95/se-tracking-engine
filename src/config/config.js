d4620c4dbbda1f44956475174007f16fb88a1db9
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-11-27 15:53:19 +0530

    Change server port 4000 to 3000

--- src/config/config.js
+++ src/config/config.js
@@ -26,7 +26,7 @@ const config = {
     },
     deployment: {
         host: process.env.HOST || '192.168.1.8',
-        port: process.env.PORT || 4000
+        port: process.env.PORT || 3000
     }
 };
 
