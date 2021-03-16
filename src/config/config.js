fe5743f0d19e0766c72275cf004ee70e4e5cf749
Aravinda Rathnayake <aravindacloud@gmail.com>
2021-03-16 15:02:39 +0530

    Add database, inverter and distributor env records

--- src/config/config.js
+++ src/config/config.js
@@ -36,17 +36,17 @@ const config = {
     },
     distributor: {
         isAllowed: true,
-        gridSuccessUrl: 'http://apps2.malindaprasad.com/home/em/send.php',
-        gridErrorUrl: 'http://apps2.malindaprasad.com/home/error',
-        pvSuccessUrl: 'http://apps2.malindaprasad.com/home/solar/live-data.php'
+        gridSuccessUrl: process.env.GRID_SUCCESS_URL,
+        gridErrorUrl: process.env.GRID_ERROR_URL,
+        pvSuccessUrl: process.env.PV_SUCCESS_URL
     },
     inverter: {
-        url: 'http://apiapp.le-pv.com:8080/api/equipDetail',
+        url: process.env.INVERTER_URL,
         serialNumber: process.env.RENAC_SN,
         email: process.env.RENAC_EMAIL
     },
     database: {
-        prodUri: 'se-tracking-engine.jw1zk.mongodb.net',
+        prodUri: process.env.PROD_URI,
         devUri: 'mongodb://127.0.0.1:27017',
         name: 'se-tracking-engine',
         user: 'admin',
